b2f5969cbbe792f5b2b59703ee16b6454be06c1b
Yermakov Dmitry <tkdmeow@gmail.com>
2023-12-17 11:35:22 +0300

    set up Loading component

---
+++ src/components/UI/Loading/style.ts
@@ -0,0 +1,12 @@
+import {css} from '@emotion/react';
+
+export const loadingtStyle = {
+  loading: css`
+    display: flex;
+    width: 600px;
+    margin: 0;
+    margin-top: 10%;
+    padding: 0;
+    justify-content: center;
+  `,
+};
